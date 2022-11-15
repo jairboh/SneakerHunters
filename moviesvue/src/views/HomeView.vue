dd0616e1ebd95768ba61d206e698105ddf82caef
Jair Olguin <jair.olguinh@gmail.com>
2022-11-14 23:43:31 -0600

    yo

---
+++ moviesvue/src/views/HomeView.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="home">
+    <section class="hero is-medium is-dark mb-6">
+        <div class="hero-body has-text-centered">
+            <p class="title mb-6">
+                Bienvenido a Sneaker Hunters
+            </p>
+            <p class="subtitle">
+                Todos los sneakers que buscas
+            </p>
+        </div>
+    </section>
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+          <h2 class="is-size-2 has-text-centered">Ultimos productos</h2>
+      </div>
+      
+      <ProductBox 
+        v-for="product in latestProducts"
+        v-bind:key="product.id"
+        v-bind:product="product" />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+import ProductBox from '@/components/ProductBox'
+
+export default {
+  name: 'HomeView',
+  data(){
+    return{
+      latestProducts: []
+    }
+  },
+  components: {
+    ProductBox
+  },
+  mounted() {
+    this.getLatestProducts()
+
+    document.title = ' SneakerHunters '
+  },
+  methods: {
+    async getLatestProducts() {
+      this.$store.commit('setIsLoading', true)
+
+      await axios
+        .get('/api/v1/latest-products/')
+        .then(response => {
+          this.latestProducts = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+
+      this.$store.commit('setIsLoading', false)
+    }
+  }
+}
+</script>
---
+++ moviesvue/src/views/Success.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="page-success">
+        <div class="columns is-multiline">
+            <div class="column is-12">
+                <h1 class="title">Gracias!</h1>
+
+                <p>Tu pedido esta siendo procesado, recibiras un correo con toda la informacion.</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Success',
+    mounted() {
+        document.title = 'Gracias por el pedido!'
+    },
+}
+</script>
